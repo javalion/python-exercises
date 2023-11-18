b0ac368646106ad8f7a8db11442f29c497ff3aa6
Terry McKee <javalion@hotmail.com>
2023-11-18 11:14:19 -0600

    Correct implementation to use the array module rather than a list

--- src/loop-array/solution.py
+++ src/loop-array/solution.py
@@ -1,4 +1,10 @@
 # Loop Array
 
-for element in [1,3,5,4,2]:
+# Import array module
+import array
+
+# Use the array function within the array module to create an integer array
+int_array = array.array('i', [1,2,3,4,5])
+
+for element in int_array:
     print(element)
